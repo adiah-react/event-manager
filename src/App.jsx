a88238ec03bdb48e5fc6ca3083c8bb0a4d6fb709
nathanieladiah <nathanieladiah@gmail.com>
2024-11-02 05:55:12 -0400

    Add login page, spinner, and useauth hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { CssBaseline } from "@mui/material";
-import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <CssBaseline enableColorScheme />
-      <div>App</div>
-    </React.Fragment>
+    // <React.Fragment>
+    //   <CssBaseline enableColorScheme />
+    //   <div>App</div>
+    // </React.Fragment>
+    <Router>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/sign-in" element={<Login />} />
+      </Routes>
+    </Router>
   );
 };
 export default App;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,108 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { MdLockOutline, MdOutlinePerson } from "react-icons/md";
+import { Navigate, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Spinner from "../../components/ui/Spinner/Spinner";
+import { auth } from "../../firebase.config";
+import useAuthStatus from "../../hooks/useAuthStatus";
+
+import "./login.scss";
+
+const Login = () => {
+  const showPassword = false;
+  const { loggedIn, checkingStatus } = useAuthStatus();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        username,
+        password
+      );
+
+      if (userCredentials.user) {
+        navigate("/home");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Invalid User Credentials");
+    }
+  };
+
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+
+  if (loggedIn) {
+    return <Navigate to="/home" />;
+  }
+
+  return (
+    <div className="login morning">
+      <div className="login__greeting">
+        Good <span className="time">morning</span>
+      </div>
+
+      <ToastContainer />
+
+      <form onSubmit={onSubmit}>
+        <div className="input-wrapper">
+          <input
+            type="text"
+            className="username-input"
+            placeholder="Username"
+            id="username"
+            value={username}
+            onChange={onChange}
+          />
+          <MdOutlinePerson />
+        </div>
+
+        <div className="input-wrapper">
+          <input
+            type={showPassword ? "text" : "password"}
+            className="passwordInput"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+          <MdLockOutline />
+        </div>
+
+        <div className="form-buttons">
+          <a href="#!" className="sign-up button">
+            Sign up
+          </a>
+          <button className="sign-in button">Sign in</button>
+        </div>
+
+        <div className="password-reset">
+          <a href="#!">Forgot Password?</a>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Login;
