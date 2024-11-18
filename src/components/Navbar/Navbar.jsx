e815a4645bedd87816e10daacd89eeed59f23775
nathanieladiah <nathanieladiah@gmail.com>
2024-11-18 09:58:11 -0400

    Add icons and style navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,27 @@
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="/oxbridge_logo.png" alt="" />
+        <span>OxisEvents</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="/pro_pic.jpg" alt="" />
+          <span>Nathaniel Adiah</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 export default Navbar;
