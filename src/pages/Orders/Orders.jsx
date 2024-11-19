23f528ffb695bf8a8d847fa909a837c2a3baef19
nathanieladiah <nathanieladiah@gmail.com>
2024-11-19 04:06:59 -0400

    Fetch the orders from firebase

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,29 +1,33 @@
-import { useState } from "react";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import Add from "../../components/Add/Add";
 import DataTable from "../../components/DataTable/DataTable";
+import Spinner from "../../components/ui/Spinner/Spinner";
+import { db } from "../../firebase.config";
 import "./orders.scss";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
+  { field: "id", headerName: "ID", width: 200 },
   {
     field: "name",
     headerName: "Name",
-    width: 100,
+    width: 200,
   },
   {
     field: "email",
     headerName: "Email",
-    width: 200,
+    width: 250,
   },
   {
     field: "phone",
     headerName: "Phone Number",
-    width: 200,
+    width: 100,
   },
   {
     field: "paid",
     headerName: "Paid",
-    width: 150,
+    width: 90,
     type: "boolean",
   },
   {
@@ -34,26 +38,60 @@ const columns = [
   },
 ];
 
-const orders = [
-  {
-    id: 1,
-    email: "nathanieljadiah@gmail.com",
-    name: "Nathaniel Adiah",
-    phone: "8684723716",
-    paid: false,
-    ticketCount: 1,
-  },
-];
-
 const Orders = () => {
   const [open, setOpen] = useState(false);
+
+  const [orders, setOrders] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // const params = useParams();
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        // Get reference
+        const ordersRef = collection(db, "orders");
+
+        // Create a query
+        const q = query(ordersRef, orderBy("timestamp", "desc"));
+
+        // Execute query
+        const querySnap = await getDocs(q);
+
+        const orders = [];
+        querySnap.forEach((doc) => {
+          return orders.push({
+            id: doc.id,
+            // data: doc.data()
+            ...doc.data(),
+          });
+        });
+
+        setOrders(orders);
+        setLoading(false);
+      } catch (error) {
+        toast.error("Could not fetch Order");
+        console.error(error);
+      }
+    };
+
+    fetchOrders();
+  });
+
   return (
     <div className="orders">
       <div className="info">
         <h1>Orders</h1>
         <button onClick={() => setOpen(true)}>Add New Order</button>
       </div>
-      <DataTable slug="orders" columns={columns} rows={orders} />
+      {loading ? (
+        <Spinner />
+      ) : orders && orders.length > 0 ? (
+        <DataTable slug="orders" columns={columns} rows={orders} />
+      ) : (
+        <p>No orders</p>
+      )}
+
       {open && <Add slug="order" columns={columns} setOpen={setOpen} />}
     </div>
   );
