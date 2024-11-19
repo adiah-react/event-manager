e0424eced933e6947a0c9631ea508559a786e032
nathanieladiah <nathanieladiah@gmail.com>
2024-11-19 14:23:44 -0400

    Edit the tickets

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -5,25 +5,12 @@ const Ticket = (id) => {
   return (
     <div className="ticket">
       <div className="code">
-        <h2>TICKET</h2>
-        <p>John Smith</p>
-        <QRCode bgColor="#25573e" fgColor="#c4961a" size={134} value={id.id} />
+        {/* <h2>TICKET</h2> */}
+        {/* <p>No. {id.id}</p> */}
+        <p></p>
+        <QRCode bgColor="#25573e" fgColor="#c4961a" size={150} value={id.id} />
       </div>
-      <div className="artwork">
-        <div className="graphic"></div>
-        <div className="text">
-          <h1 className="sr-only">Merry Grinchmas</h1>
-          <p className="sr-only">
-            Where Christmas perhaps means a little bit more
-          </p>
-          <div className="details">
-            <div className="date sr-only">7th of December | 9:30 AM</div>
-            <div className="address sr-only">
-              At Notre Dame Campus, Marabella
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* <div className="artwork"></div> */}
     </div>
   );
 };
